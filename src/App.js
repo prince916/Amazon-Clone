38f3b25967c1a2f1e9daee05d2ac3c4c5fadf108
Your Name <you@example.com>
2023-08-21 13:06:02 +0530

    update all

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>amazon clone</h1>
-      <button>click me</button>
+    <div className='w-full h-auto bg-bodyColor text-lightText'>
+      <div className='max-w-screen-xl mx-auto p-2'>
+        <Navbar />
+      </div>
     </div>
   );
 }
